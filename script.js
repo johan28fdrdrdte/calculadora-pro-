1d9e1572a45d7ea93dde7363faf18b42c2a2d48f
johan28fdrdrdte <pradojohan056@gmail.com>
2026-05-15 21:39:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,41 +14,17 @@ function calcular() {
 }
 
 function multiplicacionLarga(a, b) {
-  let num1 = a.toString();
   let num2 = b.toString().split("").reverse();
-
   let resultados = [];
 
   for (let i = 0; i < num2.length; i++) {
-    let digito = parseInt(num2[i]);
-    let parcial = (a * digito).toString() + "0".repeat(i);
+    let parcial = (a * parseInt(num2[i])).toString() + "0".repeat(i);
     resultados.push(parcial);
   }
 
-  let resultadoFinal = resultados
-    .map(n => parseInt(n))
-    .reduce((x, y) => x + y, 0)
-    .toString();
+  let resultadoFinal = resultados.reduce((x, y) => x + parseInt(y), 0);
 
-  let ancho = Math.max(
-    num1.length,
-    b.toString().length + 2,
-    ...resultados.map(r => r.length),
-    resultadoFinal.length
-  );
-
-  let pasos = "";
-
-  pasos += num1.padStart(ancho) + "\n";
-  pasos += ("× " + b).padStart(ancho) + "\n";
-  pasos += "-".repeat(ancho) + "\n";
-
-  resultados.forEach(r => {
-    pasos += r.padStart(ancho) + "\n";
-  });
-
-  pasos += "-".repeat(ancho) + "\n";
-  pasos += resultadoFinal.padStart(ancho);
+  let pasos = resultados.join("\n") + "\n------\n" + resultadoFinal;
 
   return pasos;
 }
