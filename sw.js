86237b360828387da3296e43d90767fa0ad77071
johan28fdrdrdte <pradojohan056@gmail.com>
2026-05-15 21:54:15 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,41 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = "calculadora-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon.png"
+];
+
+// INSTALAR
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', event => {
-  clients.claim();
+// ACTIVAR
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// FETCH (SIN INTERNET)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
